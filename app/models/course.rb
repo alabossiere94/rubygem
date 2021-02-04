3ac5f98c3a89c3148f6c17311eb5586112fc2214
Alex <alabossi@gmail.com>
2021-02-04 21:35:03 +0000

    change url to user friendly url

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,15 @@
 class Course < ApplicationRecord
-    validates :title,  presence: true
-    validates :description, presence: true, length: { :minimum => 5 }
+  validates :title,  presence: true
+  validates :description, presence: true, length: { :minimum => 5 }
     
-    belongs_to :user
+  belongs_to :user
     
-    def to_s
-        title 
-    end
-    has_rich_text :description
+  def to_s
+    title 
+  end
+  has_rich_text :description
+    
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  
 end
