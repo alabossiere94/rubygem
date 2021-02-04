774c0105845328013099d0b30221a8c402ed4c49
Alex <alabossi@gmail.com>
2021-02-04 22:44:50 +0000

    devise trackable

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   resources :courses
+  resources :users, only: [:index]
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
