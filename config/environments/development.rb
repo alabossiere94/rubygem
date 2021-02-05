31d62b415fabf5a8576e6d10dd55327f56d693f0
Alex <alabossi@gmail.com>
2021-02-05 01:10:38 +0000

    :Push to Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.hosts << "35bb9f79c4d4450eab5d093b96755ec5.vfs.cloud9.us-east-2.amazonaws.com"
   config.action_mailer.default_url_options = { host: 'https://35bb9f79c4d4450eab5d093b96755ec5.vfs.cloud9.us-east-2.amazonaws.com/'}
+  config.action_mailer.delivery_method = :test
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
