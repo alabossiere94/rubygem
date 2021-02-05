bdc5f84ec6fc53052b6fc8d6eab382095a845295
Alex <alabossi@gmail.com>
2021-02-05 01:35:22 +0000

    Comment out line 14

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ import "channels"
 import "bootstrap"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
-require("stylesheets/application.scss")
+// require("stylesheets/application.scss")
 
 import "@fortawesome/fontawesome-free/css/all"
 
