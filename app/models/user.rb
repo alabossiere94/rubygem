83bae08204cb899491831bb229763efe76594cb9
Alex <alabossi@gmail.com>
2021-02-18 22:17:03 +0000

    scaffold Enrollments, basic validation and design

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
          :trackable, :confirmable
 
   rolify
+  
+  has_many :courses
+  has_many :enrollments
 
   def to_s 
     email
@@ -14,8 +17,6 @@ class User < ApplicationRecord
   def username
     self.email.split(/@/).first
   end
-  
-  has_many :courses
   
   extend FriendlyId
   friendly_id :email, use: :slugged
