a1d2adb3ec5ecdd2044226f940018fdcda805608
Alex <alabossi@gmail.com>
2021-02-19 00:54:25 +0000

    button for users to enroll in courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -26,4 +26,9 @@ class Course < ApplicationRecord
   
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
+  
+  def bought(user)
+    self.enrollments.where(user_id: [user.id], course_id: [course_id].empty?)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   resources :courses do
     resources :lessons
+    resources :enrollments, only: [:new, :create]
   end
   resources :users, only: [:index, :edit, :show, :update]
   get 'home/index'
