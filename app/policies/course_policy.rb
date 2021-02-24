a561bf5c1615f06cb530cf6f17bdff16c8a282d8
Alex <alabossi@gmail.com>
2021-02-24 20:56:50 +0000

    add counter_cache across app

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -6,11 +6,11 @@ class CoursePolicy < ApplicationPolicy
   end
   
   def edit?
-    @user.has_role?(:admin) || @record.user_id == @user.id
+    @user.has_role?(:admin) || @record.user == @user
   end
   
   def update?
-    @user.has_role?(:admin) || @record.user_id == @user.id
+    @user.has_role?(:admin) || @record.user == @user
   end
   
   def new?
@@ -22,7 +22,10 @@ class CoursePolicy < ApplicationPolicy
   end
   
   def destroy?
-    @user.has_role?(:admin) || @record.user_id == @user.id
+    @user.has_role?(:admin) || @record.user == @user
   end
   
+  def owner?
+    @record.user == @user
+  end
 end
