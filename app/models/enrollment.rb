9005c8564e82bc8ff66d035231304644057b3f93
Alex <alabossi@gmail.com>
2021-03-01 22:53:06 +0000

    moved logic from controller to model using scopes

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -16,6 +16,7 @@ class Enrollment < ApplicationRecord
 
   scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
   scope :reviewed, -> { where.not(review: [0, nil, ""]) }
+  scope :latest_good_reviews, -> { limit(3).order(rating: :desc, created_at: :desc) }
 
   extend FriendlyId
   friendly_id :to_s, use: :slugged
