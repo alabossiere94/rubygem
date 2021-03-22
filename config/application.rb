b9d7594cc1d1668d90ac11cc309dee9c19c99846
Alex <alabossi@gmail.com>
2021-03-22 23:59:48 +0000

    video.js player integration

--- config/application.rb
+++ config/application.rb
@@ -5,16 +5,24 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Rubygem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    
     if Rails.env.development? #for rails-erd gem to generate a diagram
       def eager_load!
         Zeitwerk::Loader.eager_load_all
       end
+    end
+    
+    #video previews for action_text
+    config.after_initialize do
+      ActionText::ContentHelper.allowed_attributes.add 'style'
+      ActionText::ContentHelper.allowed_attributes.add 'controls'
+      
+      ActionText::ContentHelper.allowed_tags.add 'video'
+      ActionText::ContentHelper.allowed_tags.add 'audio'
+      ActionText::ContentHelper.allowed_tags.add 'source'
     end
 
     # Configuration for the application, engines, and railties goes here.
