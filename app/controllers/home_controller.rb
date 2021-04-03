743ca3ed7c58938a28fd013601fb03f30121048e
Alex <alabossi@gmail.com>
2021-04-03 05:09:07 +0000

    create a privacy policy and SEO tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, :only => [:index]
+  skip_before_action :authenticate_user!, :only => [:index, :privacy_policy]
   def index
     #@courses = Course.all.limit(3)
     @latest_good_reviews = Enrollment.reviewed.latest_good_reviews
@@ -26,6 +26,9 @@ class HomeController < ApplicationController
     else
       redirect_to root_path, alert: "You are not authorized to access this page"
     end
+  end
+  
+  def privacy_policy
   end
   
 end
